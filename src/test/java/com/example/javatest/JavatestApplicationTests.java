700686b02fab51ac26500d8662d08e437e7707ce
diqksrk <diqksrk123@naver.com>
2022-09-25 23:20:03 +0900

    feat : test annotation adding

--- src/test/java/com/example/javatest/JavatestApplicationTests.java
+++ src/test/java/com/example/javatest/JavatestApplicationTests.java
@@ -1,13 +1,43 @@
 package com.example.javatest;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class JavatestApplicationTests {
 
     @Test
-    void contextLoads() {
+    void create() {
+        JavatestApplication dataStuctureApplication = new JavatestApplication();
+
+        assertNotNull(dataStuctureApplication);
     }
 
+    @Test
+    @Disabled
+    void disableTest() {
+        System.out.println("disabled");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("AfterEach");
+    }
 }
