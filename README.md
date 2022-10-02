595dd5a4b9500daea2fd62826e1feced958bde81
diqksrk <diqksrk123@naver.com>
2022-10-03 01:13:00 +0900

    jniut 조건에 따른 테스트 실행

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 1.4) @AfterAll - 클래스 내부에 있는 테스트 전부를 실행시킨 후 딱 1번 실행되는 어노테이션
 1.5) @BeforeEach - 각 테스트 메소드를 실행하기전에 전처리 작업을 지정하는 어노테이션
 1.6) @AfterEach - 각 테스트 메소드를 실행한 후에 전처리 작업을 지정하는 어노테이션
+
+2) Junit 조건에 따른 테스트 실행
+2.1) assumeTrue() - 특정 조건을 걸어놓고 해당 조건을 통과하지 않으면, 밑에 조건들은 실행할수 없게 한다.
+2.2) assumingThat(조건, executable) - 조건을 걸어놓고, exectuable에는 () -> 값을 통한 테스트 하고 싶은 코드를 정의한다.
+2.3) @EnabledOnOs - 특정 OS에만 실행 가능한 테스트를 만들때 사용하는 어노테이션.
+2.4) @DisableOnOs - 특정 OS에만 실행 불가능한 테스트를 만들때 사용하는 어노테이션.
+2.5) @EnabledOnJre - 특정 Jre 버전에만 실행 가능 테스트를 만들때 사용하는 어노테이션.
+2.5) @DisabledOnJre - 특정 Jre 버전에만 실행 불가능한 테스트를 만들때 사용하는 어노테이션.
+2.6) @EnabledIfEnvironmentVariable - 특정 실행 환경에만 사용가능한 테스트를 만들때 사용하는 어노테이션.
+2.7) @DisabledIfEnvironmentVariable - 특정 실행 환경에만 사용 불가능한 테스트를 만들때 사용하는 어노테이션.
--- src/test/java/com/example/javatest/JavatestApplicationTests.java
+++ src/test/java/com/example/javatest/JavatestApplicationTests.java
@@ -1,23 +1,63 @@
 package com.example.javatest;
 
 import org.junit.jupiter.api.*;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.condition.*;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.time.Duration;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class JavatestApplicationTests {
 
     @Test
-    void create() {
-        JavatestApplication dataStuctureApplication = new JavatestApplication();
+    @DisplayName("자바테스트어플리케이션 만들기")
+    void create_new_javatest() {
+        Study study = new Study(-10);
 
-        assertNotNull(dataStuctureApplication);
+        assertTimeout(Duration.ofMillis(100), () -> {
+            new Study(-10);
+            Thread.sleep(300);
+        });
+        // TODO ThreadLocal -> 다른 쓰레드끼리 공유가 안된다. 기본전략 스프링의.
+
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        String message = illegalArgumentException.getMessage();
+        assertEquals(message, "limit는 0보다 커야 한다");
+
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디는 무조건 " + StudyStatus.DRAFT + "이다."),
+                () -> assertTrue(study.getLimit() > 0, () -> "스터디 참서 인원은 0보다 커야 합니다.")
+        );
+        // 람다식은 실패할경우만 연산을 하고, 그냥 메시지는 실행할때마다 연산을 실행한다.
+    }
+
+    @Test
+    @DisplayName("자바테스트 어플리케이션2 만들기")
+    // 테스트 실행할 OS 설정도 가능
+    @EnabledOnOs( { OS.MAC, OS.LINUX })
+    // 특정 자바 버전
+    @EnabledOnJre({ JRE.JAVA_8 })
+    // 실행 환경에서의 테스트
+    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "local")
+    void create_new_javatest2() {
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println(test_env);
+
+        // assumeTrue로 테스트코드를 걸면, 해당 테스트 코드를 통과하지 않는한 뒤의 테스트 코드는 실행하지 않는다.
+        assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+        // 마찬가지, 안에 있는걸 통과해야 람다 안에 있는 테스트 코드가 실행된다.
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+            System.out.println("das");
+        });
     }
 
     @Test
     @Disabled
-    void disableTest() {
+    void disable_new_javatest() {
         System.out.println("disabled");
     }
 
